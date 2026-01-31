19b8976f26ebe9852e1048239daa2b5e6f4b62a3
RosieRawr <tdgrock-x@hotmail.co.uk>
2026-01-31 22:51:23 +0000

    17

--- RSVP/rsvp.js
+++ RSVP/rsvp.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const radios = document.querySelectorAll('input[name="coming"]');
   const extraFields = document.getElementById("extraFields");
   const name = document.getElementById("guest_name");
-  const nameValue = "";
+  var nameValue = "";
 
   radios.forEach(radio => {
     radio.addEventListener("change", function () {
