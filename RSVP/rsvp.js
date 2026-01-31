acebfcfe1c4dccf2d02e8b1d85dfc51d6c0ee56f
RosieRawr <tdgrock-x@hotmail.co.uk>
2026-01-31 23:07:32 +0000

    22

--- RSVP/rsvp.js
+++ RSVP/rsvp.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       } else {
         extraFields.style.display = "none";
+        accommodation.style.display = "none";
       }
     });
   });
