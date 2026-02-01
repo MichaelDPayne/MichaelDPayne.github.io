d08cb0a7ded067b4b923c1ee972675ddfb3201e0
RosieRawr <tdgrock-x@hotmail.co.uk>
2026-02-01 16:15:05 +0000

    49

--- RSVP/rsvp.js
+++ RSVP/rsvp.js
@@ -117,7 +117,7 @@ document.getElementById("rsvpForm").addEventListener("submit", function (e) {
   .then(res => res.json())
   .then(() => {
     alert("Response saved!");
-    location.replace("RSVP/thanks.html")
+    location.replace("MichaelDPayne/MichaelDPayne.github.io/RSVP/thanks.html")
     this.reset();
   })
   .catch(err => {
